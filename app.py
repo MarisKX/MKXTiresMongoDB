70fd03de13f860330614ad03c4ec1fba107effac
MarisKX <maris.kurpnieks@gmail.com>
2021-11-28 23:50:37 +0000

    Added login functionality + differenced flash messages

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def register():
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            flash("Username already exists")
+            flash("Username already exists", category="info")
             return redirect(url_for("register"))
 
         register = {
@@ -54,8 +54,34 @@ def register():
 
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
-        flash("Registration Successful!")
+        flash("Registration Successful!", category="success")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")), category="success")
+            else:
+                # invalid password match
+                flash("Invalid Username and/or Password", category="error")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Invalid Username and/or Password", category="error")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
